605ce9c8631d1fd2ab3e43f11573ce3ab5d25470
Pedro Augusto <98715404+pedroacamargo@users.noreply.github.com>
2023-08-19 02:42:54 +0100

    Episode 4

--- code/win32_handmade.cpp
+++ code/win32_handmade.cpp
@@ -1,50 +1,99 @@
 #include <windows.h>
+#include <stdint.h>
 
 #define internal static
 #define local_persist static
 #define global_variable static
 
+typedef uint8_t uint8;
+typedef uint16_t uint16;
+typedef uint32_t uint32;
+typedef uint64_t uint64;
+
+typedef int8_t int8;
+typedef int16_t int16;
+typedef int32_t int32;
+typedef int64_t int64;
+
 global_variable bool Running;
 
 global_variable BITMAPINFO BitmapInfo;
 global_variable void* BitmapMemory;
-global_variable HBITMAP BitmapHandle;
-global_variable HDC BitmapDeviceContext;
+global_variable int BitmapWidth;
+global_variable int BitmapHeight;
+global_variable int BytesPerPixel = 4;
 
-// DIB = Device Independent Bitmap
+internal void RenderWeirdGradient(int XOffset, int YOffset){
+	int Width = BitmapWidth;
+	int Height = BitmapHeight;
+
+	// As VirtualAlloc return *void, we need to get the Bitmemory pointer
+	int Pitch = Width * BytesPerPixel;
+	uint8* Row = (uint8*)BitmapMemory;
+
+	for (int Y = 0; Y < BitmapHeight; ++Y) {
+		uint32* Pixel = (uint32 *)Row;
+		for (int X = 0; X < BitmapWidth; ++X) {
+			/*
+				Pixel in memory: BB GG RR xx -> Little endian
+			*/
+			uint8 Blue = (X + XOffset);
+			uint8 Green = (Y + YOffset);
+
+			/*
+				Memory: BB GG RR xx
+				Register: xx RR GG BB
+
+				Pixel (32-bits)
+			*/
+
+			*Pixel++ = ((Green << 8) | Blue);
+		}
+
+		Row += Pitch;
+	}
+}
+
+// DIB = Device Independent Bitmap -> It contains a color table.
+// This function is creating the memory amount for the Bitmap Frame in our window
 internal void Win32ResizeDIBSection(int Width, int Height) {
 	// TODO: Bulletproof this.
 	// Maybe don't free first, free after, then free if that fails.
-
-	// TODO: Free our DIBSection
-
-
-	if (BitmapHandle) {
-		DeleteObject(BitmapHandle);
+	if (BitmapMemory) {
+		VirtualFree(BitmapMemory, 0, MEM_RELEASE);
 	}
-	if (!BitmapDeviceContext) {
-		// TODO: Should we recreate these under certain special circumstances
-		BitmapDeviceContext = CreateCompatibleDC(0);
-	}
+	// It could be MEM_DECOMMIT instead of MEM_RELEASE
+	// MEM_RELEASE -> Will decommit the memory allocated, but free aswell
+	// MEM_DECOMMIT -> If for some reason we said "look, we don't have to pay attention to this anymore, but we're gonna actually probably want it back later"
+	// so don't bother about tracking this memory but don't get rid of it too.
+	// Also, we could change the memory protection of BitmapMemory to don't let the program write or read in the memory itself
+
+	BitmapWidth = Width;
+	BitmapHeight = Height;
+
 	BitmapInfo.bmiHeader.biSize = sizeof(BitmapInfo.bmiHeader);
-	BitmapInfo.bmiHeader.biWidth = Width;
-	BitmapInfo.bmiHeader.biHeight = Height;
+	BitmapInfo.bmiHeader.biWidth = BitmapWidth;
+	BitmapInfo.bmiHeader.biHeight = -BitmapHeight;
 	BitmapInfo.bmiHeader.biPlanes = 1;
 	BitmapInfo.bmiHeader.biBitCount = 32;
 	BitmapInfo.bmiHeader.biCompression = BI_RGB;
 
-
-	BitmapHandle = CreateDIBSection(
-		BitmapDeviceContext, &BitmapInfo,
-		DIB_RGB_COLORS,
-		&BitmapMemory,
-		0, 0);
+	int BitmapMemorySize = (BitmapWidth * BitmapHeight) * BytesPerPixel;
+	BitmapMemory = VirtualAlloc(0, BitmapMemorySize, MEM_COMMIT, PAGE_READWRITE);
 }
 
-internal void Win32UpdateWindow(HDC DeviceContext, int X, int Y, int Width, int Height) {
+internal void Win32UpdateWindow(HDC DeviceContext, RECT* ClientRect, int X, int Y, int Width, int Height) {
+
+	int WindowWidth = ClientRect->right - ClientRect->left;
+	int WindowHeight = ClientRect->bottom - ClientRect->top;
+
 	StretchDIBits(DeviceContext,
+		/*
 		X, Y, Width, Height,
 		X, Y, Width, Height,
+		*/
+		0, 0, BitmapWidth, BitmapHeight,
+		0, 0, WindowWidth, WindowHeight,
 		BitmapMemory,
 		&BitmapInfo,
 		DIB_RGB_COLORS, SRCCOPY);
@@ -88,9 +137,13 @@ LRESULT CALLBACK Win32MainWindowCallback(HWND Window, UINT Message, WPARAM WPara
 		int Y = Paint.rcPaint.top;
 		int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
 		int Width = Paint.rcPaint.right - Paint.rcPaint.left;
-		Win32UpdateWindow(DeviceContext, X, Y, Width, Height);
-		local_persist DWORD Operation = WHITENESS;
-		PatBlt(DeviceContext, X, Y, Width, Height, Operation);
+
+		RECT ClientRect;
+		GetClientRect(Window, &ClientRect);
+
+		Win32UpdateWindow(DeviceContext, &ClientRect, X, Y, Width, Height);
+		//local_persist DWORD Operation = BLACKNESS;
+		//PatBlt(DeviceContext, X, Y, Width, Height, Operation);
 
 		EndPaint(Window, &Paint);
 	} break;
@@ -120,7 +173,7 @@ int CALLBACK WinMain(
 	//WindowClass.hIcon = hInstance;
 
 	if (RegisterClassA(&WindowClass)) {
-		HWND WindowHandle = CreateWindowExA(
+		HWND Window = CreateWindowExA(
 			0,
 			WindowClass.lpszClassName,
 			"Handmade Hero",
@@ -135,16 +188,38 @@ int CALLBACK WinMain(
 			0 // Pass parameters to the window and get with the WM_CREATE
 		);
 		Running = true;
-		if (WindowHandle != NULL) {
+		if (Window != NULL) {
+			Running = true;
 			MSG Message;
+			int XOffset = 0;
+			int YOffset = 0;
 			while (Running) {
-				BOOL MessageResult = GetMessageA(&Message, 0, 0, 0);
-				if (MessageResult > 0) {
+				while (PeekMessage(&Message, 0, 0, 0, PM_REMOVE)) {
+					if (Message.message == WM_QUIT) {
+						Running = false;
+					}
+
 					TranslateMessage(&Message);
 					DispatchMessageA(&Message);
 				}
-				else break;
+				RenderWeirdGradient(XOffset, YOffset);
+
+				HDC DeviceContext = GetDC(Window);
+				RECT ClientRect;
+				GetClientRect(Window, &ClientRect);
+				int WindowWidth = ClientRect.right - ClientRect.left;
+				int WindowHeight = ClientRect.bottom - ClientRect.top;
+				Win32UpdateWindow(DeviceContext, &ClientRect, 0, 0, WindowWidth, WindowHeight);
+				ReleaseDC(Window, DeviceContext);
+				++XOffset;
+
+				//BOOL MessageResult = GetMessageA(&Message, 0, 0, 0);
+				//if (MessageResult > 0) {
+					
+				//}
+				//else break;
 			}
+
 		}
 		else {
 			//TODO(casey): Logging
