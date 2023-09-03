ede58ab853c7c719cde990239e9cc61c0e371c2b
pedroacamargo <pedroaugennes@gmail.com>
2023-09-04 00:00:43 +0100

    Interview Question

--- DataStructures/InterviewQuestions/Question1.js
+++ DataStructures/InterviewQuestions/Question1.js
@@ -39,7 +39,30 @@ console.log(containsCommonItem(['a', 'b', 'c', 'x'], ['z', 'y', 'x']));
 // Using HashTables
 // O(n) - Linear
 function containsCommonItem2(arr1, arr2) {
-    // Loop through first array and create object where properties === items in the array
-    // Loop through second array and check if second array exists on created object
+    // Can we assume always 2 parameters?
 
+    // Loop through first array and create object where properties === items in the array
+    let map = {};
+    for (let i = 0; i < arr1.length; i++) {
+        if (!map[arr1[i]]) {
+            const item = arr1[i];
+            map[item] = true;
+        }
+    }
+
+
+    // Loop through second array and check if second array exists on created object
+    for (let j = 0; j < arr2.length; j++) {
+        if (map[arr2[j]]) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+console.log(containsCommonItem2(['a', 'b', 'c', 'x'], ['z', 'y', 'x']));
+
+function containsCommonItem3(arr1, arr2) {
+    return arr1.some(item => arr2.includes(item));
 }
